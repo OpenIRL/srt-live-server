10042de8b94fcc92b3d7d817da9acab1313d93f1
René <48084558+servusrene@users.noreply.github.com>
2025-06-09 21:24:28 +0200

    updated version

--- srt-live-server.cpp
+++ srt-live-server.cpp
@@ -60,8 +60,8 @@ Server svr;
  * usage information
  */
 #define SLS_MAJOR_VERSION "1"
-#define SLS_MIN_VERSION "4"
-#define SLS_TEST_VERSION "x"
+#define SLS_MIN_VERSION "5"
+#define SLS_TEST_VERSION "0"
 static void usage()
 {
     printf("-------------------------------------------------\n");
